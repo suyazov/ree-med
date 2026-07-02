56b8f5ac860d3c6adfe40eb43691b29c46649453
TriMed Dev <dev@trimed.ru>
2026-07-02 16:35:47 +0300

    refactor: stabilize header action icon selectors

--- header.php
+++ header.php
@@ -44,7 +44,7 @@
         </button>
 
         <div class="header-actions">
-            <a href="#" class="header-action" aria-label="Сравнение">
+            <a href="#" class="header-action header-action-compare" aria-label="Сравнение">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <rect x="2" y="4" width="2" height="17" rx="1" fill="currentColor"/>
                     <rect x="8" y="5" width="2" height="15" rx="1" fill="currentColor"/>
@@ -53,13 +53,13 @@
                 </svg>
                 <span class="action-count">1</span>
             </a>
-            <a href="#" class="header-action" aria-label="Избранное">
+            <a href="#" class="header-action header-action-favorite" aria-label="Избранное">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill="currentColor" d="m12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3"/>
                 </svg>
                 <span class="action-count">1</span>
             </a>
-            <a href="#" class="header-action" aria-label="Корзина">
+            <a href="#" class="header-action header-action-cart" aria-label="Корзина">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g fill="none" fill-rule="evenodd">
                         <path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/>
@@ -68,7 +68,7 @@
                 </svg>
                 <span class="action-count">3</span>
             </a>
-            <a href="#" class="header-action search-action" aria-label="Поиск">
+            <a href="#" class="header-action header-action-search search-action" aria-label="Поиск">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill="currentColor" d="M10 4a6 6 0 1 0 0 12a6 6 0 0 0 0-12m-8 6a8 8 0 1 1 14.32 4.906l5.387 5.387a1 1 0 0 1-1.414 1.414l-5.387-5.387A8 8 0 0 1 2 10"/>
                 </svg>
